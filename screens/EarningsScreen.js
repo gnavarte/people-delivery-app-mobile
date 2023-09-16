0ae4232aa7f58a2094388efe036af058eb343844
gnavarte <gnavarte@outlook.com>
2023-09-16 20:47:43 -0300

    Ultimo cambio

--- screens/EarningsScreen.js
+++ screens/EarningsScreen.js
@@ -1,28 +1,63 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
 import travelHistoryData from '../data/travel_history.json';
+
+const windowWidth = Dimensions.get('window').width;
+
+const groupByMonth = travelHistoryData.reduce((acc, item) => {
+  const date = new Date(item.Fecha);
+  const monthYear = `${date.getMonth() + 1}-${date.getFullYear()}`;
+  if (acc[monthYear]) {
+    acc[monthYear] += item.Ganancia;
+  } else {
+    acc[monthYear] = item.Ganancia;
+  }
+  return acc;
+}, {});
+
+const months = Object.keys(groupByMonth);
+const gananciasPorMes = months.map((month) => groupByMonth[month]);
+const labels = months.map((month) => month);
+
+const dataForChart = {
+  labels: labels,
+  datasets: [
+    {
+      data: gananciasPorMes,
+    },
+  ],
+};
+
+const totalGanancias = gananciasPorMes.reduce((total, ganancia) => total + ganancia, 0);
 
 const EarningsScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.column}>Fecha</Text>
-        <Text style={styles.column}>Duración</Text>
-        <Text style={styles.column}>Valoración</Text>
-        <Text style={styles.column}>Ganancia</Text>
+      <View style={styles.centeredContent}>
+        <BarChart
+          data={dataForChart}
+          width={windowWidth}
+          height={220}
+          yAxisLabel="$"
+          chartConfig={{
+            backgroundGradientFrom: '#fff',
+            backgroundGradientTo: '#fff',
+            decimalPlaces: 2,
+            color: (opacity = 1) => `#6372ff`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+        <Text style={styles.totalText}>
+          Total de Ganancias: ${totalGanancias.toFixed(2)}
+        </Text>
       </View>
-      <FlatList
-        data={travelHistoryData}
-        keyExtractor={(item) => item.Fecha}
-        renderItem={({ item }) => (
-          <View style={styles.row}>
-            <Text style={styles.column}>{item.Fecha}</Text>
-            <Text style={styles.column}>{item.Duracion} horas</Text>
-            <Text style={styles.column}>{item.Valoracion}</Text>
-            <Text style={styles.column}>${item.Ganancia}</Text>
-          </View>
-        )}
-      />
     </View>
   );
 };
@@ -30,22 +65,16 @@ const EarningsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 10,
+  centeredContent: {
+    alignItems: 'center',
   },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 5,
-  },
-  column: {
-    flex: 1,
-    marginRight: 5,
-    fontWeight: 'bold',
+  totalText: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginTop: 16,
   },
 });
 
