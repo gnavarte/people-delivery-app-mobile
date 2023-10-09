f6ad7faf8ba6b9ae8508660c57db4a7c62728a14
gnavarte <gnavarte@outlook.com>
2023-10-09 15:30:19 -0300

    Cambios varios

--- screens/EarningsScreen.js
+++ screens/EarningsScreen.js
@@ -1,63 +1,93 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
-import { BarChart } from 'react-native-chart-kit';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import travelHistoryData from '../data/travel_history.json';
 
-const windowWidth = Dimensions.get('window').width;
-
-const groupByMonth = travelHistoryData.reduce((acc, item) => {
-  const date = new Date(item.Fecha);
-  const monthYear = `${date.getMonth() + 1}-${date.getFullYear()}`;
-  if (acc[monthYear]) {
-    acc[monthYear] += item.Ganancia;
-  } else {
-    acc[monthYear] = item.Ganancia;
-  }
-  return acc;
-}, {});
-
-const months = Object.keys(groupByMonth);
-const gananciasPorMes = months.map((month) => groupByMonth[month]);
-const labels = months.map((month) => month);
-
-const dataForChart = {
-  labels: labels,
-  datasets: [
-    {
-      data: gananciasPorMes,
-    },
-  ],
-};
-
-const totalGanancias = gananciasPorMes.reduce((total, ganancia) => total + ganancia, 0);
-
 const EarningsScreen = () => {
+
+  const navigation = useNavigation();
+
+  const navigateToTravelHistory = () => {
+    navigation.navigate('Viajes');
+  };
+
+  const today = new Date();
+  const lastWeek = new Date();
+  lastWeek.setDate(lastWeek.getDate() - 7);
+
+  const todayEarnings = calculateEarningsForDate(today);
+  const lastWeekEarnings = calculateEarningsForDateRange(lastWeek, today);
+  const averageRating = calculateAverageRating();
+  const totalTravelTime = calculateTotalTravelTime();
+
+  function calculateEarningsForDate(date) {
+    return travelHistoryData.reduce((totalEarnings, trip) => {
+      if (new Date(trip.Fecha).toDateString() === date.toDateString()) {
+        return totalEarnings + trip.Ganancia;
+      }
+      return totalEarnings;
+    }, 0);
+  }
+
+  function calculateEarningsForDateRange(startDate, endDate) {
+    return travelHistoryData.reduce((totalEarnings, trip) => {
+      const tripDate = new Date(trip.Fecha);
+      if (tripDate >= startDate && tripDate <= endDate) {
+        return totalEarnings + trip.Ganancia;
+      }
+      return totalEarnings;
+    }, 0);
+  }
+
+  function calculateAverageRating() {
+    const totalRating = travelHistoryData.reduce((total, trip) => total + trip.Valoracion, 0);
+    return (totalRating / travelHistoryData.length).toFixed(1);
+  }
+
+  function calculateTotalTravelTime() {
+    const totalMinutes = travelHistoryData.reduce((total, trip) => total + trip.Duracion, 0);
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    return `${hours}h ${minutes}m`;
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.centeredContent}>
-        <BarChart
-          data={dataForChart}
-          width={windowWidth}
-          height={220}
-          yAxisLabel="$"
-          chartConfig={{
-            backgroundGradientFrom: '#fff',
-            backgroundGradientTo: '#fff',
-            decimalPlaces: 2,
-            color: (opacity = 1) => `#6372ff`,
-            style: {
-              borderRadius: 16,
-            },
-          }}
-          style={{
-            marginVertical: 8,
-            borderRadius: 16,
-          }}
-        />
-        <Text style={styles.totalText}>
-          Total de Ganancias: ${totalGanancias.toFixed(2)}
-        </Text>
+      <Text style={styles.header}>Ganancias</Text>
+
+      <View style={styles.statsContainer}>
+        <Text style={styles.subtitle}>Hoy</Text>
+        <View style={styles.statCard}>
+          <Text style={styles.statValue}>${todayEarnings}</Text>
+          <Text style={styles.statLabel}>Ingresos</Text>
+        </View>
       </View>
+
+      <View style={styles.statsContainer}>
+        <Text style={styles.subtitle}>Última Semana</Text>
+        <View style={styles.statCard}>
+          <Text style={styles.statValue}>${lastWeekEarnings}</Text>
+          <Text style={styles.statLabel}>Ingresos</Text>
+        </View>
+      </View>
+
+      <View style={styles.additionalStatsContainer}>
+        <View style={styles.additionalStatCard}>
+          <Text style={styles.additionalStatValue}>{averageRating}</Text>
+          <Text style={styles.additionalStatLabel}>Calificación Promedio</Text>
+        </View>
+        <View style={styles.additionalStatCard}>
+          <Text style={styles.additionalStatValue}>{totalTravelTime}</Text>
+          <Text style={styles.additionalStatLabel}>Tiempo Total de Viaje</Text>
+        </View>
+      </View>
+
+      <TouchableOpacity
+        style={styles.detailButton}
+        onPress={navigateToTravelHistory}
+      >
+        <Text style={styles.detailButtonText}>Ver Detalles</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -65,16 +95,71 @@ const EarningsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#fff',
   },
-  centeredContent: {
-    alignItems: 'center',
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
   },
-  totalText: {
-    textAlign: 'center',
+  subtitle: {
     fontSize: 18,
-    marginTop: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  statsContainer: {
+    marginBottom: 16,
+  },
+  statCard: {
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  statValue: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  statLabel: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  additionalStatsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  additionalStatCard: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+    padding: 16,
+    alignItems: 'center',
+    marginRight: 16,
+  },
+  additionalStatValue: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  additionalStatLabel: {
+    fontSize: 16,
+    color: 'gray',
+    textAlign: 'center',
+  },
+  detailButton: {
+    backgroundColor: '#6372ff',
+    padding: 16,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  detailButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
