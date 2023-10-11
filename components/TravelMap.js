99c7b2958d62a2bb7320faa6a63a1a4d50289cf8
gnavarte <gnavarte@outlook.com>
2023-10-11 15:26:06 -0300

    Cambio icono marcador

--- components/TravelMap.js
+++ components/TravelMap.js
@@ -2,26 +2,25 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
 import axios from 'axios';
-import * as Location from 'expo-location'; // Importa el módulo de ubicación de Expo
+import * as Location from 'expo-location';
+import { Ionicons } from '@expo/vector-icons';
 
 const TravelMap = () => {
-  const [origin, setOrigin] = useState(); // Estado para almacenar la ubicación actual del dispositivo
+  const [origin, setOrigin] = useState();
   const [destination, setDestination] = useState({ latitude: -34.6085, longitude: -58.3705 });
   const [route, setRoute] = useState([]);
   const [routeLoaded, setRouteLoaded] = useState(false);
   const apiKey = process.env.EXPO_PUBLIC_OPENROUTESERVICE_API_KEY;
 
   useEffect(() => {
-    // Función para obtener la ubicación actual del dispositivo
     const getCurrentLocation = async () => {
       try {
         const { status } = await Location.requestForegroundPermissionsAsync();
         if (status === 'granted') {
           const location = await Location.getCurrentPositionAsync({});
           const { latitude, longitude } = location.coords;
-          setOrigin({ latitude, longitude }); // Actualiza el estado con las coordenadas del dispositivo
+          setOrigin({ latitude, longitude });
 
-          // Llama a OpenRouteService para obtener la ruta
           axios
             .get(`https://api.openrouteservice.org/v2/directions/driving-car?api_key=${apiKey}&start=${longitude},${latitude}&end=${destination.longitude},${destination.latitude}`)
             .then(response => {
@@ -31,7 +30,7 @@ const TravelMap = () => {
                 longitude: coordinate[0],
               }));
               setRoute(newCoordinates);
-              setRouteLoaded(true); // Marca la ruta como cargada
+              setRouteLoaded(true);
             })
             .catch(error => {
               console.error('Error al obtener la ruta:', error);
@@ -42,19 +41,24 @@ const TravelMap = () => {
       }
     };
 
-    getCurrentLocation(); // Llama a la función para obtener la ubicación del dispositivo
+    getCurrentLocation();
   }, [destination, apiKey]);
 
   return (
     <View style={styles.container}>
       {origin && (
-        <MapView style={styles.map} initialRegion={{
-          latitude: (origin.latitude + destination.latitude) / 2,
-          longitude: (origin.longitude + destination.longitude) / 2,
-          latitudeDelta: Math.abs(origin.latitude - destination.latitude) + 0.1,
-          longitudeDelta: Math.abs(origin.longitude - destination.longitude) + 0.1,
-        }}>
-          <Marker coordinate={origin} title="Origen" />
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: (origin.latitude + destination.latitude) / 2,
+            longitude: (origin.longitude + destination.longitude) / 2,
+            latitudeDelta: Math.abs(origin.latitude - destination.latitude) + 0.1,
+            longitudeDelta: Math.abs(origin.longitude - destination.longitude) + 0.1,
+          }}
+        >
+          <Marker coordinate={origin} title="Origen">
+            <Ionicons name="car" size={24} color="blue" />
+          </Marker>
           <Marker coordinate={destination} title="Destino" />
           {routeLoaded && <Polyline coordinates={route} strokeWidth={4} strokeColor="blue" />}
         </MapView>
